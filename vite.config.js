25e610edff35996e951b61c59fc06f16480d3b9d
annaistotts <ins17001@byui.edu>
2025-12-17 21:20:20 -0600

    fixed prompt loading in render and gitignore hopefully

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   root: "src/",
+  publicDir: "../public",
   build: {
     outDir: "../dist",
     rollupOptions: {
@@ -12,6 +13,7 @@ export default defineConfig({
         entry: resolve(__dirname, "src/entry/index.html"),
         profile: resolve(__dirname, "src/profile/index.html")
       }
-    }
+    },
+    copyPublicDir: true
   }
 });
